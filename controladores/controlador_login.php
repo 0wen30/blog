036cb6047ad0de7350d0f999d261463b4f5f74fd
Jose Angel <owen9330@gmail.com>
2021-10-26 17:22:07 -0500

    validaciones_registro

--- controladores/controlador_login.php
+++ controladores/controlador_login.php
@@ -1,5 +1,16 @@
 <?php
 
+function tieneespacios($str){
+    $str = trim($str);
+    if ($str !== '') {
+        $pattern = '/^[a-zA-Z, ]*$/';
+        if (!preg_match($pattern, $str)) {
+            return true;   
+        }
+    }
+    return false;   
+}
+
 include 'modelos/conexion.php';
 
 include 'modelos/modelo_formulario.php';
@@ -8,41 +19,22 @@ if (isset($_GET['usuario']) && isset($_GET['clave'])){
 	$us = $_GET['usuario'];
 	$cl = $_GET['clave'];
 
-/*--------------------------------------------------
-	if(Consultas::estaRegistrado($_POST['user'])){
-		    echo 'El usuario ya esta registrado';
-		}else if($_POST['pass'] !== $_POST['confirm']){
-		    echo 'Las contraseñas no coinciden';
-		}else if($_POST['pass'] == "" || $_POST['user'] == ""){
-		    echo 'Llena todos los campos';
-		}else if(tieneespacios($_POST['user'])){
-		    echo 'El nombre de usuario solo admite texto';
-		}else{
-		    Consultas::registrar($_POST['user'],$_POST['pass']);
-		    echo 'Registro realizado';
-		}
-
-		function tieneespacios($str){
-		    $str = trim($str);
-		    if ($str !== '') {
-		        $pattern = '/^[a-zA-Z, ]*$/';
-		        if (preg_match($pattern, $str)) {
-		            return true;   
-		        }
-		    }
-		    return false;   
-		}
-	--------------------------------------------------*/
-
 	$_SESSION['usuario'] = $us;
 	if(BD::comprobar($us,$cl)){
 
 		header("location: home.php");
 	}
 }else if (isset($_GET['usuarior']) && isset($_GET['claver1']) && isset($_GET['claver2'])){
-	$us = $_GET['usuarior'];
+	$us = trim($_GET['usuarior']);
 	$cl = $_GET['claver1'];
-	if(BD::nuevo_usuario($us,$cl)){
+	$cl2 = $_GET['claver2'];
+	if($cl !== $cl2){
+	    echo 'Las contraseñas no coinciden';
+	}else if($cl == "" || $us == ""){
+	    echo 'Llena todos los campos';
+	}else if(tieneespacios($us)){
+	    echo 'El nombre de usuario solo admite texto de la Aa-Zz';
+	}else if(BD::nuevo_usuario($us,$cl)){
 		header("Refresh:0");
 	}
 }
